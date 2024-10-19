b5b15ec4e676aeb1c4559011d492c259d459d170
Ariel Kriheli <arieldk6@gmail.com>
2024-10-19 15:26:00 +0300

    codeforces, mit

---
+++ codeforces/x.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main () {
+  int tests, a, n[a]; 
+  std::cin>>tests;
+
+  while (tests--) {
+    std::cin >> a;
+    std::cin >> n[a];
+    
+
+  }
+
+  return 0;
+}
