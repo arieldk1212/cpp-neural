5e0b388ff575272c0abd75995fe007173f28fe48
Ariel Kriheli <arieldk6@gmail.com>
2024-10-23 08:31:45 +0300

    stuff

--- design patterns/singleton.cpp
+++ design patterns/singleton.cpp
@@ -2,14 +2,13 @@
 #include <string>
 
 
-
 // creational pattern that lets you ensure that a class only has one instance
 // while providing a global access point to this instance using static.
 // it ensures that a class has just a single instance, it also provied a global
 // access point to that instance.
 
 
-// threa safe singleton example:
+// thread safe singleton example:
 class Singleton {
 public:
 };
