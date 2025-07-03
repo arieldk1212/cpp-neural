9a74b79b3044701ecdbd1c5b3a13d7f3a8dd25fd
Ariel Kriheli <arieldk6@gmail.com>
2025-07-03 11:08:59 +0300

    pub sub work

---
+++ CONCURRENCY/threadpool.h
@@ -0,0 +1,14 @@
+#ifndef THREADPOOL_H
+#define THREADPOOL_H
+
+#include <condition_variable>
+#include <future>
+#include <mutex>
+#include <thread>
+
+/**
+ * @brief to create our thread pool we need to first take care of a concurrent
+ * queue - add tasks, create a threadworkers data structure, execution
+ */
+
+#endif
