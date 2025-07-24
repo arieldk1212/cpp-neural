537ee56edd8928400251823e50d2d02661f2dee1
Ariel Kriheli <arieldk6@gmail.com>
2025-07-24 16:52:40 +0300

    alot of prac

--- neetcode/stuff.cpp
+++ neetcode/stuff.cpp
@@ -1,3 +1,5 @@
+#include <set>
+#include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
@@ -73,6 +75,9 @@ public:
   }
 
   void reversenosort(std::vector<int> data) {
+    // or we use 2 pointers, 0, data.size()-1, l+, r-- and for sort we use
+    // insertion sort (start at 1, save it, get prev, while prev above or zero
+    // and key smaller, prev+1 equlles prev, decrement prev, prev+1 = key)
     int n = data.size();
     for (int i = 0; i < n - 1; ++i) {
       for (int j = 0; j < n - i - 1; ++j) {
@@ -87,5 +92,81 @@ public:
       data[i] = data[n - i - 1];
       data[n - i - 1] = low;
     }
+  }
+
+  int dynamicslidwin(std::vector<int> data, int k) {
+    int l = 0;
+    int sum = 0;
+    int maxlen = 0;
+
+    for (int r = 0; r < data.size(); r++) {
+      sum += data[r];
+
+      while (sum > k) {
+        sum -= data[l];
+        l++;
+      }
+      maxlen = std::max(maxlen, r - l + 1);
+    }
+    return maxlen;
+  }
+
+  int staticslidwin(std::vector<int> data, int d, int m) {
+    int res = 0;
+    int winsum = 0;
+    // or if its sum we need to use another int, winsum = maxx, maxx =
+    // std::max(maxx, winsum);
+
+    for (int i = 0; i < m; i++) {
+      winsum += data[m];
+    }
+    if (winsum == d) {
+      res++;
+    }
+
+    for (int i = m; i < data.size(); i++) {
+      winsum += data[m] - data[i - m];
+      if (winsum == d) {
+        res++;
+      }
+    }
+
+    return res;
+  }
+
+  int dynamicslidwintwo(std::string data) {
+    int l = 0;
+    int maxlen = 0;
+    std::unordered_set<char> seen;
+
+    for (int r = 0; r < data.size(); r++) {
+      while (seen.count(data[r])) {
+        seen.erase(data[l]);
+        l++;
+      }
+      seen.insert(data[r]);
+      maxlen = std::max(maxlen, r - l + 1);
+    }
+    return maxlen;
+  }
+
+  int dynamicslidwinthree(std::string data, int k) {
+    int l = 0;
+    int maxlen = 0;
+    std::unordered_map<char, int> freq;
+
+    for (int r = 0; r < data.size(); r++) {
+      ++freq[data[r]];
+
+      while (freq.size() > k) {
+        --freq[data[l]];
+        if (freq[data[l]] == 0) {
+          freq.erase(data[l]);
+        }
+        l++;
+      }
+      maxlen = std::max(maxlen, r - l + 1);
+    }
+    return maxlen;
   }
 };
